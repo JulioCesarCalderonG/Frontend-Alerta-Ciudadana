8a51f771299cccd9337f81291c0f51dafec531a3
julio cesar calderon galindo <julcescalgalty@gmail.com>
2023-02-28 15:12:02 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendURL: 'http://192.168.100.51:4000/api',
+  backendURL: 'https://gongalsoft.com/api',
   access_token:'pk.eyJ1IjoiZ29uZ2FsMjkxMSIsImEiOiJjbGE3YWoxY3QwbHV6M3JsZHVhNGg0dXp2In0.XIAAWg7mjWj_2aJpLcCH9Q'
 };
