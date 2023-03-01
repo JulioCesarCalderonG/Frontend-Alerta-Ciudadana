19252f6ad209c61ab75060c27b5c183a43f9b089
Julio Cesar Calderon Galindo <julcescalgalty@gmail.com>
2023-03-01 16:20:36 -0500

    pruebas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendURL: 'http://192.168.1.45:4000/api',
+  backendURL: 'http://192.168.100.129:4000/api',
   access_token:'pk.eyJ1IjoiZ29uZ2FsMjkxMSIsImEiOiJjbGE3YWoxY3QwbHV6M3JsZHVhNGg0dXp2In0.XIAAWg7mjWj_2aJpLcCH9Q'
 };
