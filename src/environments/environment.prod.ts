b1521d99473b9b01ce1cefcf49751bc28efcd672
Julio Cesar Calderon Galindo <julcescalgalty@gmail.com>
2023-10-03 15:04:07 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'http://192.168.1.39:4000/api',
-  backendURL2: 'http://192.168.1.39:4000',
+  backendURL: 'http://localhost:4000/api',
+  backendURL2: 'http://localhost:4000',
   access_token:'pk.eyJ1IjoiZ29uZ2FsMjkxMSIsImEiOiJjbGE3YWoxY3QwbHV6M3JsZHVhNGg0dXp2In0.XIAAWg7mjWj_2aJpLcCH9Q'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  backendURL: 'https://gongalsoft.com/api',
+  backendURL: 'http://localhost:4000/api',
+  backendURL2: 'http://localhost:4000',
   access_token:'pk.eyJ1IjoiZ29uZ2FsMjkxMSIsImEiOiJjbGE3YWoxY3QwbHV6M3JsZHVhNGg0dXp2In0.XIAAWg7mjWj_2aJpLcCH9Q'
 };
 
