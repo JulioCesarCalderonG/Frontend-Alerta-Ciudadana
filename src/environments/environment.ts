7cdfe2d5399b19209198100b4b9e1afca85b1ccd
Julio Cesar Calderon Galindo <juliocesar.calderong@gmail.com>
2024-03-15 16:33:38 -0500

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  backendURL: 'http://localhost:4000/api',
-  backendURL2: 'http://localhost:4000',
+  backendURL: 'https://backendmuni.gongalsoft.com/api',
+  backendURL2: 'https://backendmuni.gongalsoft.com',
   access_token:'pk.eyJ1IjoiZ29uZ2FsMjkxMSIsImEiOiJjbGE3YWoxY3QwbHV6M3JsZHVhNGg0dXp2In0.XIAAWg7mjWj_2aJpLcCH9Q'
 };
 
