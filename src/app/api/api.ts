3c2167a8bda316a3c6f1b28a845b087348f7622e
Julio Cesar Calderon Galindo <julcescalgalty@gmail.com>
2025-06-11 07:49:39 -0500

    cambios

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,4 +1,4 @@
-import { environment } from "src/environments/environment";
-//import { environment } from "src/environments/environment.prod";
+//import { environment } from "src/environments/environment";
+import { environment } from "src/environments/environment.prod";
 export const pathUrl = `${environment.backendURL}/api`;
 export const pathSocket = `${environment.backendURL}`;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'https://backendmuni.gongalsoft.com/api',
+  backendURL: 'https://backendmuni.gongalsoft.com',
   backendURL2: 'https://backendmuni.gongalsoft.com',
   access_token:'pk.eyJ1IjoiZ29uZ2FsMjkxMSIsImEiOiJjbGE3YWoxY3QwbHV6M3JsZHVhNGg0dXp2In0.XIAAWg7mjWj_2aJpLcCH9Q'
 };
